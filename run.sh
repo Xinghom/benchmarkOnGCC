71e16275a6f9486d40e8d891aa6da55620ed0920
Xinghom <Xinghom@users.noreply.github.com>
2018-03-24 14:01:21 -0700

    checked

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --privileged -it xinghom/ubuntu-pmu-tools /bin/bash -c "sh run.sh"
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#docker pull down
+docker pull xinghom/ubuntu-pmu-tools
